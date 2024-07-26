58fcb277ab2038d8ed7f8f12d5a92080ce8c9f13
troner1337 <tobiasladz@gmail.com>
2024-07-26 17:21:30 +0200

    try routes

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,24 @@ use Tobiasla78\FilamentSimplePages\FilamentSimplePagesPlugin;
 use Tobiasla78\FilamentSimplePages\Livewire\SimplePage as LivewireSimplePage;
 use Tobiasla78\FilamentSimplePages\Models\SimplePage;
 
-$prefixSlug = FilamentSimplePagesPlugin::get()->getPrefixSlug();
+try {
 
-Route::get('/'.$prefixSlug.'/{slug}', function ($slug) {
+    $prefixSlug = FilamentSimplePagesPlugin::get()->getPrefixSlug();
 
-    $page = SimplePage::where('slug', $slug)->first();
+    Route::get('/'.$prefixSlug.'/{slug}', function ($slug) {
+    
+        $page = SimplePage::where('slug', $slug)->first();
+    
+        if ($page->register_outside_filament) {
+    
+            return app(LivewireSimplePage::class)->__invoke($page);
+    
+        }
+    
+        return abort(404);
+    
+    });
 
-    if ($page->register_outside_filament) {
-
-        return app(LivewireSimplePage::class)->__invoke($page);
-
-    }
-
-    return abort(404);
-
-});
+} catch(\Exception $e) {
+    //
+}
