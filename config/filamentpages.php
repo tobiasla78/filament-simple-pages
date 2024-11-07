0394a1da9a09d9cf603b9922390e412773b41edd
Étienne Chiappino <163020232+etiennechiappino@users.noreply.github.com>
2024-11-07 14:36:59 +0100

    Update and rename simple-pages.php to filamentpages.php

---
+++ config/filamentpages.php
@@ -0,0 +1,13 @@
+<?php
+
+use Tobiasla78\FilamentSimplePages\Pages\SimplePage;
+use Tobiasla78\FilamentSimplePages\Resources\SimplePageResource;
+// config for Tobiasla78/FilamentSimplePages
+return [
+    'resources' => [
+        SimplePageResource::class,
+    ],
+    'pages' => [
+        SimplePage::class,
+    ]
+];
